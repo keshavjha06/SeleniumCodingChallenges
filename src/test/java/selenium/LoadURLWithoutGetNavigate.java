107c52c08ec2da49dfcee285b8162d0f278536aa
keshav.jha <keshav.jha@medibuddy.in>
2022-07-31 12:30:27 +0530

    Added framework support

---
+++ src/test/java/selenium/LoadURLWithoutGetNavigate.java
@@ -0,0 +1,29 @@
+package selenium;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LoadURLWithoutGetNavigate {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+// We need to downcast WebDriver reference varaible to use JavascriptExecutor methods
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String url = "https://www.google.com";
+        String script = "window.location = \'" + url + "\'";
+        js.executeScript(script);
+        System.out.println(driver.getCurrentUrl());
+        String script1 = "window.location = \'http://facebook.com'";
+        js.executeScript(script1);
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().back();
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().forward();
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+    }
+}
+
---
+++ src/test/java/selenium/RefreshPage.java
@@ -0,0 +1,29 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RefreshPage {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com/");
+        driver.manage().window().maximize();
+        driver.navigate().refresh();
+
+        driver.get("https://www.facebook.com");
+        driver.get(driver.getCurrentUrl());
+
+        driver.get("https://www.yahoo.com");
+        driver.navigate().to(driver.getCurrentUrl());
+
+        driver.get("https://www.guru99.com");
+        driver.findElement(By.id("gsc-i-id2")).sendKeys(Keys.F5);
+
+        driver.close();
+
+    }
+
+}
